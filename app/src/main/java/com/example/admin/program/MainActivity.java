371dac0bf6b230ef32c5b620756b4d1197604a43
Albert <albert.alean@gmail.com>
2019-01-10 00:46:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/admin/program/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.admin.program;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText((CharSequence) editText.getText());
+            }
+        });
+    }
+
+
+}
